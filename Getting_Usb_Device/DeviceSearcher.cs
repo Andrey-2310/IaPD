bbbf95874f1ce01c65d945c466e9702b72b91ca1
Andrey-2310 <andreyredkovskiy@mail.ru>
2017-11-27 22:03:01 +0300

    getting size implemented

---
+++ Getting_Usb_Device/DeviceSearcher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using MediaDevices;
+
+namespace Getting_USB_Devices
+{
+    class DeviceSearcher
+    {
+        public List<UsbDevice> GetDevices()
+        {
+            List<UsbDevice> devices = new List<UsbDevice>();
+            List<DriveInfo> drives = DriveInfo.GetDrives().Where(d => d.IsReady && d.DriveType == DriveType.Removable).ToList();
+            List<MediaDevice> mtpDevices = MediaDevice.GetDevices().ToList();
+            foreach (MediaDevice device in mtpDevices)
+            {
+                device.Connect();
+                if (device.DeviceType != DeviceType.Generic)
+                {
+                    devices.Add(new UsbDevice(device.FriendlyName, null, null, null, true));
+                }
+            }
+            foreach (DriveInfo drive in drives)
+            {
+                devices.Add(new UsbDevice(drive.Name, BytesToMegaBytesString(drive.TotalFreeSpace),
+                    BytesToMegaBytesString(drive.TotalSize - drive.TotalFreeSpace),
+                    BytesToMegaBytesString(drive.TotalSize), false));
+            }
+            return devices;
+        }
+
+        private string BytesToMegaBytesString(long value)
+        {
+            double megaBytes = (value / 1024) / 1024;
+            return megaBytes.ToString() + " MB";
+        }
+    }
+}
---
+++ Getting_Usb_Device/USBDevice.cs
@@ -0,0 +1,20 @@
+﻿namespace Getting_USB_Devices
+{
+    class UsbDevice
+    {
+        public string Name { get; set; }
+        public string FreeSize { get; set; }
+        public string OccupiedSize { get; set; }
+        public string TotalSize { get; set; }
+        public bool IsMtp { get; set; }
+
+        public UsbDevice(string name, string freeSize, string occupiedSize, string totalSize, bool isMtp)
+        {
+            Name = name;
+            FreeSize = freeSize;
+            OccupiedSize = occupiedSize;
+            TotalSize = totalSize;
+            IsMtp = isMtp;
+        }
+    }
+}
