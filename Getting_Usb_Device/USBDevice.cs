ab0a8633bc1ae79194b73e23d71fc134ec28f521
Andrey-2310 <andreyredkovskiy@mail.ru>
2017-11-27 22:11:19 +0300

    eject device implemented

--- Getting_Usb_Device/USBDevice.cs
+++ Getting_Usb_Device/USBDevice.cs
@@ -1,4 +1,7 @@
-﻿namespace Getting_USB_Devices
+﻿using System.Linq;
+using UsbEject;
+
+namespace Getting_USB_Devices
 {
     class UsbDevice
     {
@@ -15,6 +18,15 @@
             OccupiedSize = occupiedSize;
             TotalSize = totalSize;
             IsMtp = isMtp;
+        }
+
+        public bool Eject()
+        {
+            var tempName = this.Name.Remove(2);
+            var ejectedDevice = new VolumeDeviceClass().SingleOrDefault(v => v.LogicalDrive == this.Name.Remove(2));
+            ejectedDevice.Eject(false);
+            ejectedDevice = new VolumeDeviceClass().SingleOrDefault(v => v.LogicalDrive == tempName);
+            return ejectedDevice == null;
         }
     }
 }
